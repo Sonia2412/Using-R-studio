74c7be82cca8d91eaca1a770ffb97e8ab363b1ae
Sonia W Mishra <77009741+Sonia2412@users.noreply.github.com>
2021-03-30 12:44:08 +0100

    Add files via upload

---
+++ List and objects.R
@@ -0,0 +1,7 @@
+# Creat a list of rainbow colours
+rainbow.colours<- list("Violet", "Indigo", "Blue", "Green","Yellow", "Orange", "Red")
+winter.colour<- ("White")
+spring.colour<- ("Purple")
+print(rainbow.colours)
+rm(spring.colour) 
+
