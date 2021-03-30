c5dc5be6e37c438aaf23a2f17556582464cf937d
Sonia W Mishra <77009741+Sonia2412@users.noreply.github.com>
2021-03-30 12:46:42 +0100

    Add files via upload

---
+++ User input using R.R
@@ -0,0 +1,5 @@
+# Programme to take user input
+name<- readline(prompt = "Enter name: ")
+age<- readline (prompt= "Enter age: ")
+userinput<- paste("The candidates name is", name, "and age is", age)
+print(userinput)
