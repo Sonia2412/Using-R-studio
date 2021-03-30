5e0d55e63d509825c9b1f218d4d4b4980c0571fc
Sonia W Mishra <77009741+Sonia2412@users.noreply.github.com>
2021-03-30 12:45:14 +0100

    Add files via upload

---
+++ Plot-women.R
@@ -0,0 +1,6 @@
+data()
+data(women)
+women
+women$height
+women$weight
+plot(women$height, women$weight, xlab= "Height", ylab= "Weight", main= "Comparison of Women height and weight", col="Red", type="b")
